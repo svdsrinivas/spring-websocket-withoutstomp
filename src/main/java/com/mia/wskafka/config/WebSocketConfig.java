426edcc6cb022a18a54ff7b225964adce404fc71
svdsrinivas <svdsrinivas@users.noreply.github.com>
2018-07-10 02:14:28 +0530

    updated

--- src/main/java/com/mia/wskafka/config/WebSocketConfig.java
+++ src/main/java/com/mia/wskafka/config/WebSocketConfig.java
@@ -1,8 +1,13 @@
 package com.mia.wskafka.config;
 
+import java.util.concurrent.Executors;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -10,6 +15,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @Configuration
 @ComponentScan("com.mia.wskafka")
 @EnableWebSocket
+@EnableScheduling
 public class WebSocketConfig implements WebSocketConfigurer {
 
 	@Override
@@ -26,6 +32,11 @@ public class WebSocketConfig implements WebSocketConfigurer {
 	@Bean
 	public WebSocketInteceptor getWebsocketInterceptor() {
 		return new WebSocketInteceptor();
+	}
+	
+	@Bean
+	public TaskScheduler taskScheduler() {
+	    return new ConcurrentTaskScheduler(Executors.newSingleThreadScheduledExecutor());
 	}
 
 }
