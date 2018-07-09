f21fdb410c01ee4b63f344f7f0c3e558fa34ae17
svdsrinivas <svdsrinivas@users.noreply.github.com>
2018-07-10 02:09:27 +0530

    updated

--- src/main/java/com/mia/wskafka/config/SocketHandler.java
+++ src/main/java/com/mia/wskafka/config/SocketHandler.java
@@ -1,10 +1,14 @@
 package com.mia.wskafka.config;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -28,14 +32,32 @@ public class SocketHandler extends TextWebSocketHandler {
 
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) throws InterruptedException, IOException {
-		String stoken = (String) session.getAttributes().get("stoken");
-		WebSocketSession webSocketSession = sessions.get(stoken);
 		System.out.println(message.getPayload());
 		Offer staff = new Gson().fromJson(message.getPayload(), Offer.class);
 		producer.send(staff);
-		String retValue = new Gson().toJson(storage.get(stoken),Offer.class);
-		webSocketSession.sendMessage(new TextMessage(retValue));
 	}
+	
+	@Scheduled(fixedDelay=5000)
+	public void sendOfferDetails() throws InterruptedException, IOException {
+		System.out.println("triggered");
+		if(sessions!=null && !sessions.isEmpty()) {
+			Set<String> stokens = sessions.keySet();
+			Iterator<String> ite=stokens.iterator();
+			while(ite.hasNext()) {
+				String key = ite.next();
+				String retValue = new Gson().toJson(storage.get(key),Offer.class);
+				if(retValue!=null) {
+					WebSocketSession webSocketSession = sessions.get(key);
+					if(webSocketSession.isOpen()) {
+						webSocketSession.sendMessage(new TextMessage(retValue));
+					}else{
+						ite.remove();
+					}
+				}
+			}
+		}
+	}
+	
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
